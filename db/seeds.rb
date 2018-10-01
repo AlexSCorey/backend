371da6525d804e1414ad0b282dff4b88c8bf1bad
lifeprolific <thegregoman@gmail.com>
2018-10-01 12:39:22 -0400

    added users db seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+USERS = [
+  {name: "Greg Taylor",
+    email: "greguser@doesnotexist.com",
+    password: "gregpassword",
+    phone_number: "123-456-7890"
+  },
+  {name: "Sohel Patel",
+    email: "soheluser@doesnotexist.com",
+    password: "sohelpassword",
+    phone_number: "123-456-7890"
+  },
+  {name: "Alex Corey",
+    email: "alexuser@doesnotexist.com",
+    password: "alexpassword",
+    phone_number: "123-456-7890"
+  },
+  {name: "Michael Bond",
+    email: "mikeuser@doesnotexist.com",
+    password: "mikepassword",
+    phone_number: "123-456-7890"
+  },
+]
+
+USERS.each do |user|
+  User.create!(user)
+end
