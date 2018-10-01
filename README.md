f796e66546195c86e8a3006f80f9efb2d55246cd
spatel1009 <spatel1009@gmail.com>
2018-10-01 16:04:51 -0400

    attempting to fix update user

--- README.md
+++ README.md
@@ -13,4 +13,33 @@ optional keys:
 * phone_number
 
 
+## users#edit (update)
+
+POST https://fierce-forest-56311.herokuapp.com/users/:id
+
+api_token required
+
+required keys:
+* password, minimum length: 5
+
+optional keys:
+* name
+* email, must be unique
+* phone_number
+
+
+## users#index (all users)
+
+GET https://fierce-forest-56311.herokuapp.com/users
+
+<!-- Will eventually need manager api_token -->
+
+
+## users#destroy (delete users)
+
+<!-- Will eventually need manager api_token -->
+
+DELETE https://fierce-forest-56311.herokuapp.com/users/:id
+
+
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,15 @@
 class UsersController < ApplicationController
 
+
+    def new
+        @user = User.new
+    end
+
+    def index
+        @users = User.all
+        render json: @users
+    end
+
     def create
         @user = User.new(user_params)
 
@@ -8,15 +18,38 @@ class UsersController < ApplicationController
         else
             render json: @user.errors, status: :unprocessable_entity
         end
-
     end
 
+    def edit
+        set_user
+        render json: @user
+    end
+
+    def update
+        set_user
+        if current_user.id != @user_id
+            render json: @user.errors, status: :unauthorized
+        elsif @user.update_attributes(user_params)
+            render json: @user, status: :ok
+        else
+            render json: @user.errors.messages, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        set_user
+        @user.destroy
+    end
 
 
     private
     
     def user_params
         params.permit(:name, :email, :password, :phone_number)
+    end
+
+    def set_user
+        @user = User.find(params[:id])
     end
 
 end
