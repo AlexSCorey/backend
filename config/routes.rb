c2fce1a5b910d8866f9b7864fdfe1c8dfb9daa13
spatel1009 <spatel1009@gmail.com>
2018-10-01 14:03:51 -0400

    login page working on postman

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'logins/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :users, only: [:create]
+  resources :logins, only: [:create]
   
 end
