29c4cd39c7401e54c0b52d7b66daf8cd2c161aa3
spatel1009 <spatel1009@gmail.com>
2018-10-01 11:55:26 -0400

    apitoken and pwdigest

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+
+    has_secure_password
+    has_secure_token :api_token
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: {minimum: 5}
+    validates :name, presence: true
 end
---
+++ db/migrate/20181001155227_add_pwand_token_to_user.rb
@@ -0,0 +1,8 @@
+class AddPwandTokenToUser < ActiveRecord::Migration[5.2]
+  def change
+    change_table :users do |t|
+      t.string :password_digest
+      t.string :api_token
+    end
+  end
+end
