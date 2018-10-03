ab0e5a392fb8c0ad3234b625e4aee62982ad35cd
lifeprolific <thegregoman@gmail.com>
2018-10-03 10:45:48 -0400

    refactors roles seed

--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,14 @@ class Role < ApplicationRecord
   belongs_to :user
   belongs_to :calendar
 
+  validates :user_id, presence: true
+  validates :calendar_id, presence: true
+  validates :role, presence: true
+
+  def self.add(user, calendar, type)
+    Role.new(user_id: user.id,
+      calendar_id: calendar.id,
+      role: type).save!
+  end
+
 end
