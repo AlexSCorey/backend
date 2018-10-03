e25548956c4adf345e06e426ac7fd7f1f2049139
lifeprolific <thegregoman@gmail.com>
2018-10-03 10:53:47 -0400

    added role validation list

--- app/models/role.rb
+++ app/models/role.rb
@@ -5,6 +5,8 @@ class Role < ApplicationRecord
   validates :user_id, presence: true
   validates :calendar_id, presence: true
   validates :role, presence: true
+  validates :role, inclusion: { in: %w(owner manager employee),
+    message: "%{value} is not a valid role" }
 
   def self.add(user, calendar, type)
     Role.new(user_id: user.id,
