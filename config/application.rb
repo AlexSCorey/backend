882a9974ac134fa7997ec9ac4c83f4028915fc86
lifeprolific <thegregoman@gmail.com>
2018-10-03 18:41:48 -0400

    fixed syntax error

--- config/application.rb
+++ config/application.rb
@@ -17,15 +17,16 @@ require "rails/test_unit/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.before_configuration do
-  env_file = File.join(Rails.root, 'config', 'local_env.yml')
-  YAML.load(File.open(env_file)).each do |key, value|
-    ENV[key.to_s] = value
-  end if File.exists?(env_file)
-end
-
 module Backend
   class Application < Rails::Application
+
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+      YAML.load(File.open(env_file)).each do |key, value|
+        ENV[key.to_s] = value
+      end if File.exists?(env_file)
+    end
+
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
