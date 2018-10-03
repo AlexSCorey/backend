7e4061e8f31d7a0bf955581c3ab0dee264e33c4a
lifeprolific <thegregoman@gmail.com>
2018-10-02 20:23:11 -0400

    added sendgrid gem, credentials, and mailer config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => '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',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
