7c4516f8d52e9ac2949967d53760d8aa1f00c945
lifeprolific <thegregoman@gmail.com>
2018-10-04 18:55:28 -0400

    added create and index actions and view

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,9 +1,32 @@
 class NotesController < ApplicationController
 
   def index
+    set_user
+    set_calendar
+    if @calendar.users.owners.include?(@user) ||
+      @calendar.users.managers.include?(@user)
+      @notes = Note.where(calendar_id: @calendar.id,
+        user_id: @user.id)
+      render "/notes/index.json", status: :ok
+    else
+      render json: '{}', status: :unauthorized
+    end
   end
   
   def create
+    set_user
+    set_calendar
+    if @calendar.users.include?(@user)
+      @note = Note.new({calendar_id: @calendar.id,
+        user_id: @user.id}.merge(note_params))
+      if @note.save
+        render json: @note, status: :ok
+      else
+        render json: @note.errors, status: :unprocessable_entity
+      end
+    else
+      render json: '{}', status: :unauthorized
+    end
   end
   
   def show
@@ -13,6 +36,20 @@ class NotesController < ApplicationController
   end
   
   def destroy
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def set_calendar
+    @calendar = Calendar.find(params[:calendar_id])
+  end
+
+  def note_params
+    params.permit(:text, :date)
   end
 
 end
