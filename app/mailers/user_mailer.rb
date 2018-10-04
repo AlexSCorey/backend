9089c0ab1e1b3351325a8859bb1bf3a090f2b191
lifeprolific <thegregoman@gmail.com>
2018-10-04 13:19:52 -0400

    updated sheduling app name to shiftwerk

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'GregorySDTaylor+SchedulingApp@gmail.com'
+  default from: 'GregorySDTaylor+shiftwerk@gmail.com'
 
   def welcome_email
     welcome_page = "https://scheduler-79e4e.firebaseapp.com/welcome/"
@@ -7,14 +7,14 @@ class UserMailer < ApplicationMailer
     @calendar = params[:calendar]
     @invitation = params[:invitation]
     @url  = welcome_page + params[:invitation].api_token
-    mail(to: @user.email, subject: 'Welcome to our Scheduling Site!')
+    mail(to: @user.email, subject: 'Welcome to shiftwerk!')
   end
 
   def added_email
     @user = params[:user]
     @calendar = params[:calendar]
     @url = "https://scheduler-79e4e.firebaseapp.com/login"
-    mail(to: @user.email, subject: "You've been added to a new calendar on our Scheduling Site!")
+    mail(to: @user.email, subject: "You've been added to a new calendar on shiftwerk!")
   end
 
 end
