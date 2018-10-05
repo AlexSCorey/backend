1728426aa9c08379404ed16b1d6d3a21df95da0b
lifeprolific <thegregoman@gmail.com>
2018-10-05 09:53:14 -0400

    added custom validation for note date

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,4 +1,15 @@
 class Note < ApplicationRecord
   belongs_to :user
   belongs_to :calendar
+
+  validates :text, length: { minimum: 1 }, presence: true
+  validate :date_format_valid
+
+  private
+
+  def date_format_valid
+    unless date
+      errors.add(:date, "date format could not be parsed")
+    end
+  end
 end
