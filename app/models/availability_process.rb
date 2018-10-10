0d94d396ce658d9bfae3880ddacc26314fd66263
lifeprolific <thegregoman@gmail.com>
2018-10-10 12:31:32 -0400

    added dependent destroy and seed method on process

--- app/models/availability_process.rb
+++ app/models/availability_process.rb
@@ -1,5 +1,34 @@
 class AvailabilityProcess < ApplicationRecord
   belongs_to :calendar
-  has_many :availability_requests
+  has_many :availability_requests, dependent: :destroy
   has_many :availability_responses, through: :availability_requests
+
+  def self.seed(calendar_id, start_date, end_date, chance)
+    process = AvailabilityProcess.new(calendar_id: calendar_id,
+      start_date: start_date,
+      end_date: end_date)
+    process.save!
+
+    process.calendar.users.distinct.each do |user|
+      request = AvailabilityRequest.new(
+        user_id: user.id,
+        availability_process_id: process.id,
+        complete: true)
+      request.save!
+
+      shifts = Shift.where(
+        calendar_id: calendar_id,
+        start_time: start_date.beginning_of_day .. end_date.end_of_day)
+
+      shifts.each do |shift|
+        response = AvailabilityResponse.new(
+          availability_request_id: request.id,
+          shift_id: shift.id,
+          available: (rand() < chance))
+        response.save!
+      end
+    end
+    return process
+  end
+
 end
--- app/models/availability_request.rb
+++ app/models/availability_request.rb
@@ -2,5 +2,5 @@ class AvailabilityRequest < ApplicationRecord
   has_secure_token :api_token
   belongs_to :user
   belongs_to :availability_process
-  has_many :availability_responses
+  has_many :availability_responses, dependent: :destroy
 end
