fa06d169c2b851a9e028d3e03c090e389bf53973
lifeprolific <thegregoman@gmail.com>
2018-10-13 15:57:40 -0400

    changed shiftwerk to shiftgear in emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: 'GregorySDTaylor+shiftwerk@gmail.com'
+  default from: 'GregorySDTaylor+shiftgear@gmail.com'
 
   def welcome_email
     welcome_page = "https://scheduler-79e4e.firebaseapp.com/welcome/"
@@ -7,14 +7,14 @@ class UserMailer < ApplicationMailer
     @calendar = params[:calendar]
     @invitation = params[:invitation]
     @url  = welcome_page + params[:invitation].api_token
-    mail(to: @user.email, subject: 'Welcome to shiftwerk!')
+    mail(to: @user.email, subject: 'Welcome to shiftgear!')
   end
 
   def added_email
     @user = params[:user]
     @calendar = params[:calendar]
     @url = "https://scheduler-79e4e.firebaseapp.com/login"
-    mail(to: @user.email, subject: "You've been added to a new calendar on shiftwerk!")
+    mail(to: @user.email, subject: "You've been added to a new calendar on shiftgear!")
   end
 
   def forgot_password
